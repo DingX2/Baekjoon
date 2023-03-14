092e4e4da8ce85048315830d1561feeac16fbf3a
DingX2 <96682768+DingX2@users.noreply.github.com>
2023-03-15 04:36:04 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
@@ -0,0 +1,11 @@
+
+def main(data):
+    answer = 0
+    data /= 4
+    for i in range(int(data)):
+        print("long", end=" ")
+    print("int")
+    return answer
+
+data = int(input())
+main(data)
