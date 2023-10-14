093bfded3405b75dd9d0a35296a24b59429846f0
DingX2 <96682768+DingX2@users.noreply.github.com>
2023-10-15 01:33:51 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 332 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,25 @@
+import sys
+def is_balanced(ary):
+    stack = []
+    for char in ary:
+        if char in '([':
+            stack.append(char)
+        elif char in ')]':
+            if not stack:
+                return "no"
+            if (char == ')' and stack[-1] == '(') or (char == ']' and stack[-1] == '['):
+                stack.pop()
+            else:
+                return "no"
+
+    if len(stack) == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+while True:
+    word = input().rstrip()
+    if word == '.':  # . 이 들어오면 종료
+        break
+    #word = input().strip()
+    print(is_balanced(word))
