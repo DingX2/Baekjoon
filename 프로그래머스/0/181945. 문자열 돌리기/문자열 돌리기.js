5354b3412e9218e30f0db65894d767ea86097159
DevXuan <96682768+DingX2@users.noreply.github.com>
2024-04-26 15:07:37 +0900

    [level 0] Title: 문자열 돌리기, Time: 70.05 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    for (const item of str){
+        console.log(item)
+    } 
+});
