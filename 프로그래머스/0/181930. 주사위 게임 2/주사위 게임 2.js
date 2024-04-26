396788ba444ba07a268778c6b4d4d0e6aeaca040
DevXuan <96682768+DingX2@users.noreply.github.com>
2024-04-26 16:12:52 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,8 +1,10 @@
 solution = (a, b, c) => {
-    if (a !== b && b !== c && c !== a )
-        return a + b + c
-    else if (a === b && b === c && c === a) 
-        return (a+b+c) * (a**2 + b**2 + c**2) * (a**3+b**3+c**3)
-    else
-      return (a+b+c) * (a**2 + b**2 + c**2)
+    const sets = new Set([a, b, c]);
+    if (sets.size === 1){
+        return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
+    }else if (sets.size === 2){
+        return (a+b+c)*(a**2+b**2+c**2)
+    }else{
+        return a+b+c
+    }
 }
