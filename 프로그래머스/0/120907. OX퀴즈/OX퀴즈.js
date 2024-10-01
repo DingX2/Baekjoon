14e1c58333b0da0a23084098af05c9d04ed251cd
DevXuan <96682768+DingX2@users.noreply.github.com>
2024-10-01 16:03:50 +0900

    [level 0] Title: OX퀴즈, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,13 +1,23 @@
-function solution(quiz) {
-    const answer = [];
-    for(let i =0; i<quiz.length; i++){
-        const temp = quiz[i].split(" ");
-        switch(temp[1]){
-            case '+':
-                answer.push(+temp[0] + +temp[2] === +temp[4] ? "O" : "X"); break;   
-            case '-':
-                answer.push(+temp[0] - +temp[2] === +temp[4] ? "O" : "X");
-        }
-    }
-    return answer;
+const solution = (quiz)=> {
+    return quiz.map((string) => {
+        const [n1, sign, n2, _, result] = string.split(" ");
+        return sign === "+" && (+result=== +n1 + +n2) || 
+            sign === "-" && (+result === +n1 - +n2) ? "O" : "X"
+    })
 }
+
+// 구조분해할당을 사용할 것 => 0
+// 코드 반복을 줄일 수는 없나? => answer.push.... => && 연산을 사용해보자
+        // switch(sign){
+        //     case '+':
+        //         answer.push(+n1 + +n2 === +result ? "O" : "X"); break;   
+        //     case '-':
+        //         answer.push(+n1 - +n2 === +result ? "O" : "X");
+        // }
+// for map 을 사용하면 return 문을 줄일 수 있겠다 => O
+    // for(let i =0; i<quiz.length; i++){
+    //     const [n1, sign, n2, _, result] = quiz[i].split(" ");
+    //     return sign === "+" && (result=== +n1 + +n2) || 
+    //         sign === "-" && (result === +n1 - +n2) ? "O" : "X"
+    // }
+// -로 인한 문제는 없을까?
