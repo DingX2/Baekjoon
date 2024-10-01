8b00a0b2e5dcf129ceb4f15d612e1da62fa3435d
DevXuan <96682768+DingX2@users.noreply.github.com>
2024-10-01 22:44:50 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,14 +1,12 @@
 const solution = (keyinput, b) => {
     let answer = [0, 0];
+    const [x,y] = [b[0]/2-1, b[1]/2-1];
     for (const dir of keyinput) {
-        if (dir === "left" && answer[0] > -Math.floor(b[0] / 2)) {
-            answer[0] -= 1;
-        } else if (dir === "right" && answer[0] < Math.floor(b[0] / 2)) {
-            answer[0] += 1;
-        } else if (dir === "down" && answer[1] > -Math.floor(b[1] / 2)) {
-            answer[1] -= 1;
-        } else if (dir === "up" && answer[1] < Math.floor(b[1] / 2)) {
-            answer[1] += 1;
+        switch(dir){
+            case "left" : if (answer[0] > -x) answer[0] -= 1; break;
+            case "right" : if (answer[0] < x) answer[0] += 1; break;
+            case "up" : if (answer[1] < y) answer[1] += 1; break;
+            case "down" : if (answer[1] > -y) answer[1] -= 1;
         }
     }
     return answer;
