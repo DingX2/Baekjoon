ae29a290e942c4f99d103a9f095a8d0a9b4b8cee
DevXuan <96682768+DingX2@users.noreply.github.com>
2024-10-01 22:58:04 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,4 +1,11 @@
+// filter를 활용한 풀이
+// const solution = (id_pw, db) => {
+//     const ary = db.filter(([id, pwd]) => id === id_pw[0])
+//     return ary.length ? (ary.filter(([id, pwd]) => pwd === id_pw[1]).length ? "login" : "wrong pw") : "fail";
+// }
+
+// Map 객체를 활용한 풀이
 const solution = (id_pw, db) => {
-    const ary = db.filter(([id, pwd]) => id === id_pw[0])
-    return ary.length ? (ary.filter(([id, pwd]) => pwd === id_pw[1]).length ? "login" : "wrong pw") : "fail";
+    const maps = new Map(db);
+    return maps.has(id_pw[0]) ? maps.get(id_pw[0]) === id_pw[1] ? "login": "wrong pw"  : "fail"
 }
