7b77bebd63f2a029610db164af84b2315abd2f72
DevXuan <96682768+DingX2@users.noreply.github.com>
2024-10-01 22:01:40 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,7 +1,9 @@
-const solution = (chicken, answer=0) => {
-    while (chicken >= 10){
-        answer += Math.floor(chicken/10);
-        chicken = Math.floor(chicken/10) + chicken%10;
-    }
-    return answer;
-}
+// const solution = (chicken, answer=0) => {
+//     while (chicken >= 10){
+//         answer += Math.floor(chicken/10);
+//         chicken = Math.floor(chicken/10) + chicken%10;
+//     }
+//     return answer;
+// }
+// 진법 계산으로 표현할 수 있음 9번 먹으면, 1개 
+const solution = (chicken) => parseInt((chicken-1)/9);
