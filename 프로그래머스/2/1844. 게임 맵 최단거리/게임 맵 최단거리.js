d801b4dee0a23bd33779d0ae89a7bb696ecd5788
DevXuan <96682768+DingX2@users.noreply.github.com>
2025-03-07 01:36:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 54.28 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,31 +1,29 @@
-const solution = (maps) => {
-    const queue = new Array(maps.length * maps[0].length * 3);
+const solution = (maps, level=0) => {
+    //bfs
+    const queue = [[0,0,0]];
+    const dir = [ [0,1], [-1,0], [0,-1], [1,0] ];
+    //const visited = Array.from({ length: maps.length }, () => Array(maps[0].length).fill(false));
     let front = 0, rear = 0;
-
-    queue[rear++] = 0;  // x
-    queue[rear++] = 0;  // y
-    queue[rear++] = 1;  // level (초기값 1)
-
-    const dir = [[0,1], [-1,0], [0,-1], [1,0]];
-
+    queue[rear++] = [0, 0, 0];
+    // level check
     while (front < rear) {
-        const x = queue[front++];
-        const y = queue[front++];
-        const level = queue[front++];
-
-        if (x === maps.length - 1 && y === maps[0].length - 1) {
-            return level;
+        const [x, y, level] = queue[front++];
+        
+        if (x === maps.length-1 && y === maps[0].length-1){
+            return level+1;
         }
-
-        for (const [dx, dy] of dir) {
-            const nx = x + dx, ny = y + dy;
-            if (nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length && maps[nx][ny] === 1) {
-                queue[rear++] = nx;
-                queue[rear++] = ny;
-                queue[rear++] = level + 1;
-                maps[nx][ny] = 0;  // 방문 처리
+        
+        for (const [dx, dy] of dir){
+            const [nx, ny] = [dx+x, dy+y];
+            if (nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length && maps[nx][ny] === 1){
+                maps[nx][ny] = 0;
+                queue[rear++] = [nx, ny, level+1];
             }
         }
     }
     return -1;
-};
+}
+
+// const validation = (nx, ny, maps) => {
+//     return 0<=nx && nx<maps.length && 0<=ny && ny<maps[0].length && maps[nx][ny] === 1;
+// }
