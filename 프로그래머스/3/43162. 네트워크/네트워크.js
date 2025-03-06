f92f5d6b94e1a7b2a701dcef157c2616ff210a81
DevXuan <96682768+DingX2@users.noreply.github.com>
2025-03-07 02:39:24 +0900

    [level 3] Title: 네트워크, Time: 0.70 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,32 +1,35 @@
-solution = (n, computers) => {
-    const parent = Array.from({length: n}, (_, i) => i);
-    
-    const find = (x) =>{
-        if(parent[x] !== x){
-            parent[x] = find(parent[x]);
+function solution(n, computers) {
+    const parent = Array.from({ length: n }, (_, i) => i);
+
+    function find(x) {
+        if (parent[x] !== x) {
+            parent[x] = find(parent[x]); // 경로 압축
         }
         return parent[x];
     }
-    
-    const union = (a, b) =>{
-        const rootA= find(a);
+
+    function union(a, b) {
+        const rootA = find(a);
         const rootB = find(b);
-        if (rootA !== rootB){
-            parent[rootB] = rootA;
+        if (rootA !== rootB) {
+            parent[rootB] = rootA; // 작은 값이 부모가 되도록
         }
     }
-    
-    for (let i=0; i<n; i++){
-        for (let j=0; j<n; j++){
-            if(computers[i][j] ===1){
-                union(i,j);
+
+    // 네트워크 연결 정보 적용
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (computers[i][j] === 1) {
+                union(i, j);
             }
         }
     }
-    const networks = new Set();
+
+    const correctSet = new Set();
     for (let i = 0; i < n; i++) {
-        networks.add(find(i));
+        correctSet.add(find(i));
     }
 
-    return networks.size;
+    return correctSet.size;
 }
+
