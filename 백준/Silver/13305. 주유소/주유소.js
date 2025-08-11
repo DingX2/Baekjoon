5bb80ba99d9257b526fe538bcd16fbac5a79f25f
DevXuan <96682768+DingX2@users.noreply.github.com>
2025-08-12 02:53:56 +0900

    [Silver III] Title: 주유소, Time: 272 ms, Memory: 45656 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,23 +1,25 @@
 // copilot: disable
 const fs = require("fs");
-//const input = fs.readFileSync("input2.txt").toString().trim().split("\n");
- const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+//const input = fs.readFileSync("input.txt").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const solution = () => {
   const N = +input[0];
-  const dist = input[1].split(" ").map(Number);
-  const cities = input[2]
+  const dist = input[1].split(" ").map((x) => BigInt(x));
+  const price = input[2]
     .split(" ")
-    .map(Number)
+    .map((x) => BigInt(x))
     .slice(0, N - 1);
 
-  let total_cost = 0;
-  let min_cost_city = Infinity;
+  let total_cost = 0n;
+  let min_cost = price[0];
 
-  for (const [index, city] of cities.entries()) {
-    min_cost_city = Math.min(min_cost_city, cities[index]);
-    total_cost += dist[index] * min_cost_city;
+  for (let i = 0; i < N - 1; i++) {
+    if (price[i] < min_cost) {
+      min_cost = price[i];
+    }
+    total_cost += dist[i] * min_cost;
   }
 
-  return total_cost;
+  return total_cost.toString();
 };
 console.log(solution());
