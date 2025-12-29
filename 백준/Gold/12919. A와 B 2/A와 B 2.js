1c50da6bad1424720646a3734e9e8d11265f57e3
DevXuan <96682768+DingX2@users.noreply.github.com>
2025-12-30 03:26:29 +0900

    [Gold V] Title: A와 B 2, Time: 92 ms, Memory: 9380 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const solution = (input) => {
+  const S = input[0];
+  const T = input[1];
+  let answer = 0;
+
+  const dfs = (current) => {
+    if (current === S) {
+      answer = 1;
+      return;
+    }
+    if (current.length === 0) return;
+
+    if (current[current.length - 1] === "A") {
+      dfs(current.slice(0, -1));
+    }
+
+    if (current[0] === "B") {
+      dfs([...current.slice(1)].reverse().join(""));
+    }
+  };
+
+  dfs(T);
+  return answer;
+};
+console.log(solution(input));
