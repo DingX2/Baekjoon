6d73967b7da6dfa7b764993e0652f7dccf25f88f
DevXuan <96682768+DingX2@users.noreply.github.com>
2026-03-05 03:10:23 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 56.71 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,38 +1,4 @@
-const solution = (gems) => {
-    let answer = [];
-    
-    const kindsOfGems = new Set(gems)
-    const gemsMap = new Map();
-    
-    let bestLength = Infinity;
-    let bestStart = Infinity;
-    let bestEnd = Infinity;
-    
-    const max = gems.length;
-    let [left, right] = [0, 0];
-    while(left<max && right<max){
-        // gem 늘리기
-        gemsMap.set(gems[right], (gemsMap.get(gems[right])||0) +1);
-        
-        //조건이 불합할때까지
-        while (gemsMap.size === kindsOfGems.size){
-            answer.push([left+1, right+1])
-            
-            let currentLength = (right-left);
-            if( (currentLength < bestLength) || 
-               currentLength === bestLength && left < bestStart) {
-                bestLength = currentLength;
-                bestStart = left;
-                bestEnd = right
-            }
-            gemsMap.set(gems[left], (gemsMap.get(gems[left])||0)-1)
-            if(gemsMap.get(gems[left]) === 0) { 
-                gemsMap.delete(gems[left]); 
-            }
-            left +=1; 
-        }
-        right+=1;
-    }
-    
-    return [bestStart+1, bestEnd+1];
+function solution(gems) {
+    var answer = [];
+    return answer;
 }
